1ed249c20e07f2c2af5d83ab23396ad15e4be0b8
Ayushman Mishra <ayushmanmishra094@gmail.com>
2023-07-13 18:31:18 +0530

    updated

--- python_basic/index.py
+++ python_basic/index.py
@@ -1428,4 +1428,57 @@ p1.myfunc()
 # The self Parameter
 # The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
 
-print("The self Parameter")
+class Person:
+   def __init__(mysillyobject, name, age):
+      mysillyobject.name = name
+      mysillyobject.age = age
+   def myfunc(abc):
+      print("Hello my name is " + abc.name)
+p1 = Person("John", 36)
+p1.myfunc()
+
+
+# Modify Object Properties
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+p1 = Person("John", 36)
+p1.age = 40
+print(p1.age)
+
+# Delete Object Properties
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+p1 = Person("John", 36)
+# del p1.age
+print(p1.age)
+
+#  Delete Object
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+p1 = Person("John", 36)
+# del p1
+print(p1)
+
+# The pass Statement
+class Person:
+  pass
+#  having an empty class definition like this, would raise an error without the pass statement
+
+
+# Python Inheritance
+# Inheritance allows us to define a class that inherits all the methods and properties from another class.
+# Parent class is the class being inherited from, also called base class.
+# Child class is the class that inherits from another class, also called derived class.
+
